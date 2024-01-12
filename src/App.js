5d4b8f4d59d2849c4a8d075a0dbcdab1b5da0854
Alaudhin <ralaudhin1@gmail.com>
2024-01-12 09:53:49 +0530

    react pro

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./App.css";
+import ManageBkgs from "./Components/ManageBkgs";
+import { Route, Routes, BrowserRouter, Link, NavLink } from "react-router-dom";
+import notification from "./images/active.png";
+import userImg from "./images/user.png";
+import completed from "./images/checked.png";
+import cancelled from "./images/close.png";
+import upcoming from "./images/new.png";
+import discountLogo from "./images/discount.png";
+import CompanyLogo from "./images/lessgo logo1.jpg";
+import downArrow from "./images/down-arrow.png";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div id="navBar">
+        <div id="logo">
+          <img src={CompanyLogo} alt="Company Logo" />
+        </div>
+        <div id="bar-icons">
+          <ul>
+            <li>
+              <img src={discountLogo} title="Look For Offers" alt="Discount Logo" />
+            </li>
+            <li>
+              <img src={notification} title="You Have One Notification" alt="Notification" />
+            </li>
+            <li id="history">
+              My Bookings
+              <span id="downArrow">
+                <img src={downArrow} alt="Down Arrow" />
+              </span>
+              <ul id="i-image">
+                <li>
+                  <img src={completed} alt="Completed Trips" />Completed Trips
+                </li>
+                <li>
+                  <img src={upcoming} alt="Upcoming Trips" />Upcoming Trips
+                </li>
+                <li>
+                  <img src={cancelled} alt="Cancelled Trips" />Cancelled Trips
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink to="/ManageBks" style={{ textDecoration: "none", color: "white" }}>
+                Manage Bookings
+              </NavLink>
+            </li>
+            <li id="userImg">
+              <img src={userImg} title="User Login" alt="User Login" />
+              <ul>
+                <li>Login</li>
+                <li>New User?</li>
+              </ul>
+              <img src={downArrow} id="ddarrow" alt="Down Arrow" />
+             
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/ManageBks" element={<ManageBkgs />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/Components/ManageBkgs.js
@@ -0,0 +1,88 @@
+import "./ManageBkgs.css";
+import React, { useRef } from "react";
+import bus1 from "./Booking Images/Bus1.jpg";
+import PNR from "./Booking Images/ticket.png";
+import FirstName from "./Booking Images/id-card.png";
+import Cancel from "./Booking Images/close.png";
+import { useNavigate } from "react-router";
+
+export default function ManageBkgs() {
+  const navigate = useNavigate();
+  const myTicketRef = useRef(null);
+  const myNameRef = useRef(null);
+
+  const chhecKIt = () => {
+    const myTicket = myTicketRef.current;
+    const myName = myNameRef.current;
+
+    if (myTicket.value=="") {
+      alert("Please Fill The Valid Ticket Number");
+      return;
+    }
+    if (myName.value=="") {
+      alert("Please Fill The Matching Name");
+      return;
+    }
+    if (/\d/.test(myName.value)) {
+      alert("Name Should Only Contain Letters");
+    }
+    if (!/^\d+$/.test(myTicket.value)) {
+      alert("Ticket Should only Contains Number");
+    }
+  };
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div id="container" style={{ position: "relative" }}>
+        <div id="design">
+          <img src={bus1} alt="Bus" />
+        </div>
+        <div id="content">
+          <div id="cancel">
+            <img src={Cancel} onClick={navigateHome} alt="Cancel" />
+          </div>
+          <div id="textContent">
+            <h2>Manage Bookings</h2>
+            <br />
+            <h5>
+              Enter The Details Below And Please Help Us To retrieve The Ticket
+              For You{" "}
+            </h5>
+          </div>
+          <div className="merge">
+            <img src={PNR} alt="PNR" />
+            <input
+              type="text"
+              id="myTicket"
+              ref={myTicketRef}
+              placeholder="Enter Your Ticket Number"
+            />
+          </div>
+          <div className="merge">
+            <img src={FirstName} alt="FirstName" />
+            <input
+              type="text"
+              id="myName"
+              ref={myNameRef}
+              placeholder="Enter Your firstName"
+            />
+          </div>
+          <div id="footers">
+            <button id="bookBtn" onClick={chhecKIt}>
+              Get Booking
+            </button>
+            <div id="textcontent">
+              <h6>
+                Already have An Account?<a href="#">Sign Up</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
